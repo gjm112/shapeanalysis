8f5719d6abc0b79d9173b96ce64d574af47882b5
gjm112 <gjm112@gmail.com>
2019-01-10 09:41:03 -0600

    Made sure to use mode = "C" in calc_shape_dist

--- R/impute_partial_shape.R
+++ R/impute_partial_shape.R
@@ -1,39 +1,9 @@
-library(fdasrvf)
-load("/Users/gregorymatthews/Dropbox/StatisticalShapeAnalysis/data_set_of_full_teeth.RData")
+
 ######################################
 #This function takes in a list of potential donors and one partial shape 
 #It then returns M completed shapes
 ######################################
-partial_shape <- t(ptsTrainList[[1]][[1]][11:42,])
-complete_shape_list <- lapply(ptsTrainList[[1]], t)
 
-start <- Sys.time()
-test <- impute_partial_shape(complete_shape_list,partial_shape, M = 25, k = 10)
-end <- Sys.time()
-end-start
-
-plot(t(test$imputed[[1]]),type="l", col = rgb(0,0,0,0), xlim = c(-0.4, 0.15), ylim = c(-0.2,0.2), lwd = 3)
-points(t(red$donor),col = "red", type = "l",lwd=1, lty = 3)
-set.seed(1234)
-for (i in 1:50){
-  u <- runif(3)
-  points(t(test$imputed[[i]]),type="l",col =  rgb(u[1],u[2],u[3],0.5), lwd = 3)
-}
-red <- complete_partial_shape(t(ptsTrainList[[1]][[1]]),t(ptsTrainList[[1]][[1]][11:42,]))
-
-arr <- array(0, dim = c(2,199,25))
-for (i in 1:25){
-  arr[,,i] <- test$imputed[[i]]
-}
-
-mn_shape <- apply(arr,c(1,2),mean)
-points(t(mn_shape), type = "l", col = rgb(0.1,0.2,0.7), lwd = 2)
-
-
-
-
-source("/Users/gregorymatthews/Dropbox/shapeanalysisgit/R/calc_shape_dist_partial.R")
-source("/Users/gregorymatthews/Dropbox/shapeanalysisgit/R/complete_partial_shape.R")
 
 impute_partial_shape <- function(complete_shape_list, partial_shape, k = 10, M = 5){
   
