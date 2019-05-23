81bc1a1636d5d3b6538e6f18655e97726c1c0e7e
gjm112 <gjm112@gmail.com>
2019-05-23 10:15:49 -0500

    g

--- R/simulation_script_for_server_parameterized.R
+++ R/simulation_script_for_server_parameterized.R
@@ -24,7 +24,14 @@
 ### tail -f /home/gmatthews1/shapeAnalysis/R/simulation_script1.Rout
 
 ###Rscript --verbose /Users/gregorymatthews/Dropbox/shapeanalysisgit/R/simulation_script_for_server_parameterized.R 1 > /Users/gregorymatthews/Dropbox/shapeanalysisgit/check.Rout
-###R CMD BATCH '--args d=1' R/simulation_script_for_server_parameterized.R & 
+###R CMD BATCH '--args d=3' R/simulation_script_for_server_parameterized.R & 
+
+
+#Something like this????
+# for ((d = 4; d <= 5; d++));
+# do R CMD BATCH '--args d=${d}' R/simulation_script_for_server_parameterized.R ;
+# done;
+
 
 
 
