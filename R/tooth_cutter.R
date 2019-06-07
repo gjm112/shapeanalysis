f37fd072e1484e1c18d64e1b3a8a790ee147c2ae
gjm112 <gjm112@gmail.com>
2019-06-07 11:13:29 -0500

    There were two points that had the exact same distance at the smallest point.  So I just picked the first one to be cut point.

--- R/tooth_cutter.R
+++ R/tooth_cutter.R
@@ -19,7 +19,9 @@ tooth_cutter <- function(tooth) {
   suitable_points <- which(tooth_distance == min(lowest_distance(tooth_distance), na.rm = 1), arr.ind = 1)
   
   cut_points <- suitable_points[which(diff(t(suitable_points)) >= 20 & diff(t(suitable_points)) <= 40),]
-  
+  if (nrow(cut_points) >1){
+    cut_points <- cut_points[1,]
+  }
   # Creates new data sets for the tooth in question, depending on if they are between the points in question or not
   # For the set which removes points (tooth_left in this case), the numbers are adjusted so that the closest points are included
   #tooth_left <- tooth[-c((cut_points[1] + 1) : (cut_points[2] - 1)), ] #Grady's original code
