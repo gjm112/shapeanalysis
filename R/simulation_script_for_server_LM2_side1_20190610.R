888ecdabe38b0513338fc01dafb97095f6ebf8e1
gjm112 <gjm112@gmail.com>
2019-06-13 11:07:54 -0500

    LM2 side 1 and 2.

--- R/simulation_script_for_server_LM2_side1_20190610.R
+++ R/simulation_script_for_server_LM2_side1_20190610.R
@@ -24,7 +24,7 @@ k <- 5
 side <- 1 #could be 1 or 2..
 tooth <- "LM2"
 
-file <- paste0("./results/results20190525_side=",side,"_k=",k,"_M=",M,"_tooth=",tooth,".RData")
+file <- paste0("./results/results20190610_side=",side,"_k=",k,"_M=",M,"_tooth=",tooth,".RData")
 load(file)
 #/home/gmatthews1/shapeAnalysis
 
