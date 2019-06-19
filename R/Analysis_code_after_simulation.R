6a259723d08b1c5d2ed60256330df0223eaab48b
gjm112 <gjm112@gmail.com>
2019-06-19 16:43:44 -0500

    Analysis code after simulation reverted.

--- R/Analysis_code_after_simulation.R
+++ R/Analysis_code_after_simulation.R
@@ -7,63 +7,28 @@ bovids <- gs_url("https://docs.google.com/spreadsheets/d/1KGkTVz5IVuBdtQie0QBdeH
 bovids.df <- bovids %>% gs_read(ws = 1)
 subset(bovids.df, `Tooth Type` == "LM1")
 
-
-#load("/home/gmatthews1/shapeAnalysis/results/results20190220.RData")
-#load("./results20190220.RData")
-load("./results20190503_side=1_k=5_M=5_tooth=LM1.RData")
-load("/Users/gregorymatthews/Dropbox/shapeanalysisgit/results/results20190503_side=1_k=5_M=5_tooth=LM1.RData")
-
-part <- results_list[[1]]$imputed_partial_shape$imputed[[1]]
-whole <- resamplecurve(t(ptsTrainList[[tooth]][[d]]),199)
-calc_shape_dist(part,whole)
-results_list[[1]]$dist[1,]
-
-results_list[[1]]$dist[order(results_list[[1]]$dist$V4),][1:10,]
-
-# for (i in 1:length(results_list)){
-# results_list[[i]]$dist <- results_list[[i]]$dist[-grep("Error",results_list[[i]]$dist$V1),]
-# results_list[[i]]$dist$V1 <- as.numeric(as.character(results_list[[i]]$dist$V1))
-# results_list[[i]]$dist$V2 <- as.numeric(as.character(results_list[[i]]$dist$V2))
-# results_list[[i]]$dist$V3 <- as.numeric(as.character(results_list[[i]]$dist$V3))
-# results_list[[i]]$dist$V4 <- as.numeric(as.character(results_list[[i]]$dist$V4))
-# results_list[[i]]$dist$V5 <- as.numeric(as.character(results_list[[i]]$dist$V5))
-# }
-
-
-DSCN3732 
-DSCN2616 
-plot(scale(ptsTrainList[["LM1"]][["DSCN3732"]], center = TRUE, scale = FALSE), xlim = c(-500,500), asp = 1, col = "red", type = "l")
-points(scale(ptsTrainList[["LM1"]][["DSCN2616"]], center = TRUE, scale = FALSE), col = "blue", type = "l")
-points(t(results_list[["DSCN2871"]]$imputed_partial_shape[[1]][[1]]), col = "orange", type = "l")
-
-part <- results_list[["DSCN2871"]]$imputed_partial_shape[[1]][[1]]
-whole <- t(ptsTrainList[["LM1"]][["DSCN3732"]])
-whole <- resamplecurve(whole, N = 199, mode = "C")
-calc_shape_dist(part,whole,mode = "C")
-
-points((scale(t(whole),center = TRUE, scale = FALSE)), type = "l", col = "red")
-
-part <- results_list[["DSCN2871"]]$imputed_partial_shape[[1]][[1]]
-whole <- t(ptsTrainList[["LM1"]][["DSCN2616"]])
-whole <- resamplecurve(whole, N = 199, mode = "C")
-calc_shape_dist(part,whole,mode = "C")
-
-points((scale(t(whole),center = TRUE, scale = FALSE)), type = "l", col = "blue")
-
+load("/Users/gregorymatthews/Dropbox/shapeanalysisgit/results/results20190610_side=1_k=5_M=5_tooth=UM3.RData")
 
 logloss_imputed <- c() 
 logloss_part <- c()
+#logloss_imputed_mean <- c()
 for (knn in 1:20){print(knn)
 
 ids <- names(results_list)
-DSCN <- ids[1]
 
 knn_partial_matching <- function(DSCN){
 temp <- results_list[[DSCN]]$dist_partial
 
-dat <- data.frame(t(data.frame(c(table(temp$tribe[order(temp$dist)][1:knn])/knn))))
+temp$Tribe <- factor(temp$Tribe, levels = unique(sort(temp$Tribe)))
+
+dat <- data.frame(t(data.frame(c(table(temp$Tribe[order(temp$dist)][1:knn])/knn))))
+
+#Weighted KNN
+#wts <- c(table(temp$Tribe[order(temp$dist)][1:knn])/table(temp$Tribe))
+#dat <- data.frame(t(data.frame((wts/sum(wts)))))
 row.names(dat) <- NULL
-dat$true <- results_list[[DSCN]]$truth$tribe[1]
+dat$true <- results_list[[DSCN]]$truth$Tribe[1]
+dat$DSCN <- DSCN
 return(dat)
 }
 
@@ -89,18 +54,27 @@ part_match_df$true_pred_prob[i] <- part_match_df[i,as.character(part_match_df$tr
 #Now for the imputed teeth
 knn_imputed <- function(DSCN){
   temp <- results_list[[DSCN]]$dist
+  
+  temp$Tribe <- factor(temp$Tribe, levels = unique(sort(temp$Tribe)))
+  
   dat_list <- list()
   for (i in 1:M){
-  pro <- data.frame(t(data.frame(c(table(temp$tribe[order(temp[[paste0("V",i)]])][1:knn])/knn))))
+  pro <- data.frame(t(data.frame(c(table(temp$Tribe[order(temp[[paste0("V",i)]])][1:knn])/knn))))
+  
+  #Weighted KNN
+  #wts <- c(table(temp$Tribe[order(temp[[paste0("V",i)]])][1:knn])/table(temp$Tribe))
+  #pro <- data.frame(t(data.frame((wts/sum(wts)))))
   row.names(pro) <- NULL
   dat_list[[i]] <- pro
   }
   df <- do.call(rbind,dat_list)
   dat <- data.frame(t(data.frame(unlist(apply(df,2,mean)))))
   row.names(dat) <- NULL
-  dat$true <- results_list[[DSCN]]$truth$tribe[1]
+  dat$true <- results_list[[DSCN]]$truth$Tribe[1]
+  dat$DSCN <- DSCN
   return(dat)
 }
+
 
 imputed_match <- lapply(ids, knn_imputed)
 imputed_match_df <- do.call(rbind,imputed_match)
@@ -110,24 +84,49 @@ for (i in 1:nrow(imputed_match_df)){
   imputed_match_df$true_pred_prob[i] <- imputed_match_df[i,as.character(imputed_match_df$true[i])] 
 }
 
+#Now for mean imputed shape
+# knn_imputed_mean <- function(DSCN){
+#   temp <- results_list[[DSCN]]$imputed_partial_shape$dist_mean
+#   
+#   temp$Tribe <- factor(temp$Tribe, levels = unique(sort(temp$Tribe)))
+#   
+#   
+#   dat <- data.frame(t(data.frame(c(table(temp$Tribe[order(temp$dist)][1:knn])/knn))))
+#   row.names(dat) <- NULL
+#   dat$true <- results_list[[DSCN]]$truth$tribe[1]
+#   return(dat)
+#   
+# }
+# 
+# imputed_mean_match <- lapply(ids, knn_imputed_mean)
+# imputed_mean_match_df <- do.call(rbind,imputed_mean_match)
+# 
+# imputed_mean_match_df$true_pred_prob <- NA
+# for (i in 1:nrow(imputed_mean_match_df)){
+#   imputed_mean_match_df$true_pred_prob[i] <- imputed_mean_match_df[i,as.character(imputed_mean_match_df$true[i])] 
+# }
 
 #table(imputed_match_df$true_pred_prob, part_match_df$true_pred_prob)
 
-
-logloss_imputed[knn] <- mean(-log(imputed_match_df$true_pred_prob+0.0001))
-logloss_part[knn] <- mean(-log(part_match_df$true_pred_prob+0.0001))
+#logloss_imputed_mean[knn] <- mean(-log(imputed_mean_match_df$true_pred_prob+(10^-12)))
+logloss_imputed[knn] <- mean(-log(imputed_match_df$true_pred_prob+(10^-12)))
+logloss_part[knn] <- mean(-log(part_match_df$true_pred_prob+(10^-12)))
 
 }
+
+save(list = c("logloss_imputed","logloss_part","imputed_match_df","part_match_df"), 
+file = "/Users/gregorymatthews/Dropbox/shapeanalysisgit/results/results20190610_side=1_k=5_M=5_tooth=UM3_summaries.RData")
+
+apply(imputed_match_df[,1:7],2,sum)
+apply(part_match_df[,1:7],2,sum)
+table(imputed_match_df$true)
+
+
 
 fret <- data.frame(logloss = c(logloss_imputed,logloss_part), k = c(1:20,1:20), method = c(rep("knn w Imputation",20),rep("knn no imputation",20)))
 library(ggplot2)
 ggplot(aes(x = k, y = logloss, col = method), data = fret) +  geom_point() + geom_line()
 
-plot(1:20, logloss_imputed, type = "l", col = "red", ylim = c(0,7))
-points(1:20, logloss_part, type = "l", col = "blue")
-
-points(1:20, logloss_imputed, pch = 16, col = "red")
-points(1:20, logloss_part, pch = 16, col = "blue")
 
 #Predict the class
 imputed_match_df$pred <- names(imputed_match_df[,1:7])[apply(imputed_match_df[,1:7],1,which.max)]
@@ -135,4 +134,36 @@ table(factor(imputed_match_df$true,levels = c("Alcelaphini", "Antilopini", "Trag
 
 part_match_df$pred <- names(part_match_df[,1:7])[apply(part_match_df[,1:7],1,which.max)]
 table(factor(part_match_df$true,levels = c("Alcelaphini", "Antilopini", "Tragelaphini", "Neotragini","Bovini", "Reduncini", "Hippotragini" )),factor(part_match_df$pred, levels = c("Alcelaphini", "Antilopini", "Tragelaphini", "Neotragini","Bovini", "Reduncini", "Hippotragini" )))
+
+plot(imputed_match_df$true_pred_prob,part_match_df$true_pred_prob, xlim = c(0,1), ylim = c(0,1))
+
+imputed_match_df$true_pred_prob
+
+#imputed_mean_match_df$pred <- names(imputed_mean_match_df[,1:7])[apply(imputed_mean_match_df[,1:7],1,which.max)]
+#table(factor(imputed_mean_match_df$true,levels = c("Alcelaphini", "Antilopini", "Tragelaphini", "Neotragini","Bovini", "Reduncini", "Hippotragini" )),factor(imputed_mean_match_df$pred, levels = c("Alcelaphini", "Antilopini", "Tragelaphini", "Neotragini","Bovini", "Reduncini", "Hippotragini" )))
+
+#ROC analysis 
+for (q in c("Alcelaphini","Antilopini","Bovini","Hippotragini","Neotragini","Reduncini","Tragelaphini")){
+temp <- imputed_match_df
+tribe <- q
+temp$true <- as.character(temp$true)
+temp$true[(temp$true) != tribe] <- 0
+temp$true[(temp$true) == tribe] <- 1
+
+library(ROCR)
+pred <- prediction(temp[[tribe]], as.numeric(temp$true))
+perf <- performance(pred, measure = "tpr", x.measure = "fpr") 
+plot(perf, col="red", main = tribe)
+
+temp <- part_match_df
+temp$true <- as.character(temp$true)
+temp$true[(temp$true) != tribe] <- 0
+temp$true[(temp$true) == tribe] <- 1
+
+library(ROCR)
+pred <- prediction(temp[[tribe]], as.numeric(temp$true))
+perf <- performance(pred, measure = "tpr", x.measure = "fpr") 
+plot(perf, col="blue", add = TRUE, lty = 3)
+
+}
 
