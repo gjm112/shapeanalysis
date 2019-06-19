b961cf67c73818729e7c86580a5fbb689f75ca9b
gjm112 <gjm112@gmail.com>
2019-06-19 14:24:19 -0500

    LM2 side 1 m 10 k 10

--- R/simulation_script_for_server_LM2_side1_20190610_k10_M10.R
+++ R/simulation_script_for_server_LM2_side1_20190610_k10_M10.R
@@ -13,6 +13,14 @@
 # chmod +x /home/gmatthew/Work/shapeanalysis/shape_script.sh
 # qsub -A SE_HPC -t 720 -n 1 -q pubnet /home/gmatthew/Work/shapeanalysis/shape_script.sh
 
+#!/usr/bin/bash
+
+nohup R CMD BATCH --vanilla /home/gmatthew/Work/shapeanalysis/R/simulation_script_for_server_LM2_side1_20190610_k5_M5.R /home/gmatthew/Work/shapeanalysis/simulation_script_for_server_LM2_side1_20190610_k5_M5.Rout
+
+# chmod +x /home/gmatthew/Work/shapeanalysis/shape_script_UM1_2_k5_M5.sh
+# qsub -A SE_HPC -t 720 -n 1 -q pubnet /home/gmatthew/Work/shapeanalysis/shape_script_UM1_2_k5_M5.sh
+
+
 
 
 start_all <- Sys.time()
@@ -21,7 +29,7 @@ library(parallel)
 
 M <- 10
 k <- 10
-side <- 1 #could be 1 or 2
+side <- 1 #could be 1 or 2.
 tooth <- "LM2"
 
 file <- paste0("./results/results20190610_side=",side,"_k=",k,"_M=",M,"_tooth=",tooth,".RData")
