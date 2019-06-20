c7b21cede0a5cc27020e5ce90f38912b0a0fc798
gjm112 <gjm112@gmail.com>
2019-06-20 10:24:12 -0500

    UM2 side 1 and 2 k10 M10

--- R/simulation_script_for_server_UM2_side1_20190610_k10_M10.R
+++ R/simulation_script_for_server_UM2_side1_20190610_k10_M10.R
@@ -15,10 +15,10 @@
 
 #!/usr/bin/bash
 
-# nohup R CMD BATCH --vanilla /home/gmatthew/Work/shapeanalysis/R/simulation_script_for_server_LM3_side2_20190610_k5_M5.R /home/gmatthew/Work/shapeanalysis/simulation_script_for_server_LM3_side2_20190610_k5_M5.Rout
-# 
-# chmod +x /home/gmatthew/Work/shapeanalysis/shape_script_UM1_1_k10_M10.sh
-# qsub -A SE_HPC -t 720 -n 1 -q pubnet /home/gmatthew/Work/shapeanalysis/shape_script_UM1_1_k10_M10.sh
+#nohup R CMD BATCH --vanilla /home/gmatthew/Work/shapeanalysis/R/simulation_script_for_server_LM2_side2_20190610_k20_M20.R /home/gmatthew/Work/shapeanalysis/simulation_script_for_server_LM3_side1_20190610_k20_M20.Rout
+
+# chmod +x /home/gmatthew/Work/shapeanalysis/shape_script_UM3_1_k20_M20.sh
+# qsub -A SE_HPC -t 720 -n 1 -q pubnet /home/gmatthew/Work/shapeanalysis/shape_script_UM3_1_k20_M20.sh
 
 
 
