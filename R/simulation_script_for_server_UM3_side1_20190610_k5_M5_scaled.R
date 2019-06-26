6e8a327b459d0218b9021c167eb1ebdb070981e7
gjm112 <gjm112@gmail.com>
2019-06-26 15:23:32 -0500

    scaled stuff

--- R/simulation_script_for_server_UM3_side1_20190610_k5_M5_scaled.R
+++ R/simulation_script_for_server_UM3_side1_20190610_k5_M5_scaled.R
@@ -17,8 +17,8 @@
 
 #nohup R CMD BATCH --vanilla /home/gmatthew/Work/shapeanalysis/R/simulation_script_for_server_UM1_side1_20190610_k5_M5_scaled.R /home/gmatthew/Work/shapeanalysis/simulation_script_for_server_UM1_side1_20190610_k5_M5_scaled.Rout
 
-# chmod +x /home/gmatthew/Work/shapeanalysis/shape_script_LM2_2_k20_M20.sh
-# qsub -A SE_HPC -t 720 -n 1 -q pubnet /home/gmatthew/Work/shapeanalysis/shape_script_LM2_2_k20_M20.sh
+# chmod +x /home/gmatthew/Work/shapeanalysis/shape_script_LM2_1_k5_M5_scaled.sh
+# qsub -A SE_HPC -t 720 -n 1 -q pubnet /home/gmatthew/Work/shapeanalysis/shape_script_LM2_1_k5_M5_scaled.sh
 
 
 
@@ -33,7 +33,7 @@ side <- 1 #could be 1 or 2.
 tooth <- "UM3"
 scale <- TRUE
 
-file <- paste0("./results/results20190610_side=",side,"_k=",k,"_M=",M,"_tooth=",tooth,".RData")
+file <- paste0("./results/results20190610_side=",side,"_k=",k,"_M=",M,"_tooth=",tooth,"scaled.RData")
 load(file)
 #/home/gmatthews1/shapeAnalysis
 
